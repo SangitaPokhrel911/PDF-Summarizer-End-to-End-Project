119d38d3e22046de4735045675caa17a12dfd535
Sangita Pokhrel <106988913+SangitaPokhrel911@users.noreply.github.com>
2024-02-03 23:35:04 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from dotenv import load_dotenv
+import streamlit as st
+import time
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+
+
+# Sidebar contents
+with st.sidebar:
+    st.title('💬PDF Summarizer and Q/A App')
+    st.markdown('''
+    ## About this application
+    You can built your own customized LLM-powered chatbot using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(2)
+    st.write(' Why drown in papers when your chat buddy can give you the highlights and summary? Happy Reading. ')
+    add_vertical_space(2)    
+    st.write('Made by ***Sangita Pokhrel***')
+
+def main():
+    load_dotenv()
+
+    #Main Content
+    st.header("Ask About Your PDF 🤷‍♀️💬")
+
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF File and Ask Questions", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      with st.chat_message("user"):
+        st.write("Hello World 👋")
+      user_question = st.text_input("Please ask a question about your PDF here:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+
+    
+
+if __name__ == '__main__':
+    main()
