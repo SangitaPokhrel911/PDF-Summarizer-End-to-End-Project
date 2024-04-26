1b7b0bc1e494afef2773468964f038f2a939381a
chm10 <christian.maekawa@gmail.com>
2024-04-26 16:13:32 -0300

    Update environment to install project using Poetry. Updated requirements and remove deprecated functions. Create modules to avoid recalle API without new asks. Debug with what was inputed, asked and output

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import time
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
-
-
+from langchain_openai import OpenAI
+from langchain_community.callbacks import get_openai_callback
+from langchain_openai import OpenAIEmbeddings
 # Sidebar contents
 with st.sidebar:
     st.title('💬PDF Summarizer and Q/A App')
@@ -26,10 +24,31 @@ with st.sidebar:
     st.write(' Why drown in papers when your chat buddy can give you the highlights and summary? Happy Reading. ')
     add_vertical_space(2)    
     st.write('Made by ***Sangita Pokhrel***')
+def process_pdf(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
 
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=2000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return text, chunks
+
+def create_knowledge_base(chunks):
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+    return knowledge_base
+
+knowledge_base = None
+old_pdf = None
 def main():
     load_dotenv()
-
+    global knowledge_base, old_pdf
     #Main Content
     st.header("Ask About Your PDF 🤷‍♀️💬")
 
@@ -39,38 +58,32 @@ def main():
     
     # extract the text
     if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      with st.chat_message("user"):
-        st.write("Hello World 👋")
-      user_question = st.text_input("Please ask a question about your PDF here:")
-      if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-        
+      if knowledge_base is None:
+        pdf_reader = PdfReader(pdf)
+        text, chunks = process_pdf(pdf)
+        knowledge_base = create_knowledge_base(chunks)
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+        old_pdf = pdf
+      else:
+        if pdf != old_pdf:
+           knowledge_base = None
+      if knowledge_base:
+        # show user input
+        with st.chat_message("user"):
+          st.write("Hello World 👋")
+        user_question = st.text_input("Please ask a question about your PDF here:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+
+            with get_openai_callback() as cb:
+                response = chain.invoke( {"input_documents":docs, "question": user_question} )
+                print(cb)
+
+            st.write(response)
 
     
 
